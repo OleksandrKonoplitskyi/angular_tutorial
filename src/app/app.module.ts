7219dc01ae36af583ce2a75a1dcb96443ef1f0d2
Oleksandr Konoplitskyi <konoplitsky91@gmail.com>
2017-09-10 23:09:42 +0300

    hero tour

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent, Hero} from './app.component';
+
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
